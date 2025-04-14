24293387d45a205b54d370e36dec114a353ab91e
PhazonicRidley <ma13hew@gmail.com>
2025-04-13 21:45:35 -0700

    Optimized delay_ms

--- Lab5/E1/Delay Function Implementation/main.c
+++ Lab5/E1/Delay Function Implementation/main.c
@@ -44,7 +44,7 @@
  *            |                  |
  *            |                  |
  *            |                  |
- * Author: 
+ * Author:
 *******************************************************************************/
 /* DriverLib Includes */
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
@@ -53,39 +53,56 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <math.h>
 
-#define CURRENT_TIME ((uint32_t)MAP_Timer32_getValue(TIMER32_0_BASE))
+#define CURRENT_TIME_0 ((uint32_t)MAP_Timer32_getValue(TIMER32_0_BASE))
+#define CURRENT_TIME_1 ((uint32_t)MAP_Timer32_getValue(TIMER32_1_BASE))
 
-void delay_ms(uint32_t count)
+
+inline void delay_ms(uint32_t count)
 {
+    uint32_t prev = MAP_Timer32_getValue(TIMER32_0_BASE);
     uint64_t ticks_to_wait = ((uint64_t)MAP_CS_getMCLK() * count) / 1000;
     uint64_t total_elapsed = 0;
-    uint64_t prev = (uint64_t)CURRENT_TIME;
-    uint64_t current;
+    uint32_t current;
 
     while(total_elapsed < ticks_to_wait)
     {
-        uint64_t elapsed;
-        current = (uint64_t)CURRENT_TIME;
+        current = MAP_Timer32_getValue(TIMER32_0_BASE);
 
         if(prev >= current) {
-            elapsed = prev - current;
+            total_elapsed += (uint64_t)(prev - current);
         } else {
-            elapsed = prev + ((uint32_t)0xFFFFFFFF - current); // Handle underflow
+            total_elapsed += (uint64_t)(prev + ((uint32_t)0xFFFFFFFF - current)); // Handle underflow
         }
 
-        total_elapsed += elapsed;
         prev = current;
     }
 }
 
 
+static inline uint64_t delta_time_us(uint64_t start, uint64_t end) {
+
+    uint32_t tick_delta = start - end;
+//    if (end <= start) {
+//
+//    } else {
+//        tick_delta = start + ((uint32_t)0xFFFFFFFF - end);
+//    }
+
+    uint64_t us = ((uint64_t)tick_delta * 1000000) / MAP_CS_getMCLK();
+    return us;
+}
+
+
+#define SIZE 10
+
 int main(void)
 {
-    const uint32_t size = 10;
-    uint32_t t0, t1, delay;
-    uint64_t freq = 3000;
-    uint32_t delay_v[size] = {5000, 2000, 1000, 50, 20, 10, 5, 2, 1, 0};
+    uint32_t t0, t1;
+    uint32_t delay_v[SIZE] = {5000, 2000, 1000, 50, 20, 10, 5, 2, 1, 0};
+    uint64_t delay_data[SIZE];
+
     int i;
     /* Stop Watchdog  */
     MAP_WDT_A_holdTimer();
@@ -96,13 +113,21 @@ int main(void)
     MAP_Timer32_initModule(TIMER32_1_BASE, TIMER32_PRESCALER_1, TIMER32_32BIT,TIMER32_FREE_RUN_MODE);
     MAP_Timer32_startTimer(TIMER32_1_BASE, 0);
 
-    for(i = 0; i < size; i++ )
-    {
-        t0 = MAP_Timer32_getValue(TIMER32_1_BASE);
+
+    for (i = 0; i < SIZE; i++) {
+        t0 = (uint64_t)CURRENT_TIME_1;
         delay_ms(delay_v[i]);
-        t1 = MAP_Timer32_getValue(TIMER32_1_BASE);
-        delay = (t0-t1)/freq;
-        printf("Delay time at %d: %d\n", delay_v[i], delay);
-        printf("Delay time difference at %d: %d\n", delay_v[i], delay - delay_v[i]);
+        t1 = (uint64_t)CURRENT_TIME_1;
+        delay_data[i] = delta_time_us(t0, t1);
     }
+
+    for (i = 0; i < SIZE; i++) {
+        uint64_t expected_us = (uint64_t)delay_v[i] * 1000;
+        uint64_t measured_us = delay_data[i];
+        uint64_t error_us = abs(expected_us - measured_us);
+        printf("Delay expected value: %lums, Measured value: %lluus, Error delta: %lluus\n", delay_v[i], delay_data[i], error_us);
+    }
+
+    while(1);
+
 }
